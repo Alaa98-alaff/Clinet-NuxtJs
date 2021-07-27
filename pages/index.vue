2687471679c3054bcfa2ef5736c678c2fa123932
Alaa Alaff <travel98_alaff@hotmail.com>
2021-07-28 00:49:47 +0300

    SmallCardDisplayComponent & SmallCardComponent have been Done

--- pages/index.vue
+++ pages/index.vue
@@ -6,15 +6,25 @@
       :key="cardInfo.id"
       :cardsSection="cardInfo"
     />
+
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
   </div>
 </template>
 
 <script>
 import { largeCardSections } from "@/assets/data.js";
+import { smallCardSections } from "@/assets/data.js";
 
 export default {
   data() {
-    return { largeCardInfo: largeCardSections };
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    };
   }
 };
 </script>
