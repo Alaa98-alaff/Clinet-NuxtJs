2ab78755e37115e3fe5511d3cea2e48323f54a0e
Alaa Alaff <travel98_alaff@hotmail.com>
2021-07-28 02:47:05 +0300

    Nav Component has been Created

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="container">
-    <Hero />
-    <LargeCardDisplay
-      v-for="cardInfo in largeCardInfo"
-      :key="cardInfo.id"
-      :cardsSection="cardInfo"
-    />
+  <div>
+    <Nav />
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
 
-    <SmallCardDisplay
-      v-for="cardInfo in smallCardInfo"
-      :key="cardInfo.id"
-      :cardsSection="cardInfo"
-    />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
   </div>
 </template>
 
